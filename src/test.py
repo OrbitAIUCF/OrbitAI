3076e355fb6538b509fbb3ae1a760ee483f2d48d
Adam Mouedden <146101324+Adammouedden@users.noreply.github.com>
2025-02-10 22:31:15 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,7 +1,10 @@
 class newClass:
+  init():
+    print("Test")
 
-print("Hello this is a test Hello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a test")
 
+
+print("Hello this is a test Hello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is a testHello this is ")
 
 
 
